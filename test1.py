635f5b3dcb90121a5b10c330ec070eedb38e0174
Bidemi Olaide <haramidey239@gmail.com>
2023-08-31 11:54:17 +0100

    update

--- test1.py
+++ test1.py
@@ -3,13 +3,13 @@ import main
 import unittest
 
 class TestMain(unittest.TestCase):
-    
+
     def test_addition(self):
         self.assertEqual(main.addition(1, 2), 3, "Should be 3")
-        self.assertEqual(main.addition(5, 5), 10, "Should be 10")
-        self.assertEqual(main.addition(40, 20), 60, "Should be 60")
-        self.assertEqual(main.addition(-3, 2),-1,"Should be -1")
-    def test_subraction(self):
-        self.assertEqual(main.subtraction(5,4), 1, "Should be 1")
+        self.assertEqual(main.addition(10, 7), 17, "Should be 17")
+        self.assertEqual(main.addition(8, 22), 30, "Should be 30")
+        self.assertEqual(main.addition(-5, 9), 4, "Should be 4")
+    def test_subtraction(self):
+        self.assertEqual(main.subtraction(8, 2), 6, "Should be 6")
 if __name__ == '__main__':
     unittest.main()
